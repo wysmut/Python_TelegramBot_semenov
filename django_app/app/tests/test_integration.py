6a3f605958261eb1b30efd8e8eaf80a362dcc809
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:16:33 +0700

    Update test_integration.py

--- django_app/app/tests/test_integration.py
+++ django_app/app/tests/test_integration.py
@@ -1,1 +1,7 @@
+from django.test import TestCase
+from .models import Event
 
+class IntegrationTest(TestCase):
+    def test_full_flow(self):
+        event = Event.objects.create(title="Test", date="2023-01-01", user_id=1)
+        self.assertEqual(Event.objects.count(), 1)
