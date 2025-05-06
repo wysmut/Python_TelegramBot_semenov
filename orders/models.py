4f9c3b6a9ea1e629365cd8833af9e21b150c275e
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 18:05:53 +0700

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -1,1 +1,9 @@
+from django.db import models
 
+class Order(models.Model):
+    description = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.description
