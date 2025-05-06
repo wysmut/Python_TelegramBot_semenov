8fd58ed0da0a34163ab13ee48480a7e838e3dd9a
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:21:59 +0700

    Update models.py

--- bot/models/models.py
+++ bot/models/models.py
@@ -1,9 +1,12 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, DateTime
 from bot.utils.database import Base
+from datetime import datetime
 
 class Event(Base):
     __tablename__ = 'events'
     id = Column(Integer, primary_key=True)
-    title = Column(String)
+    title = Column(String(100))
+    description = Column(String(500))
     date = Column(Date)
+    created_at = Column(DateTime, default=datetime.utcnow)
     user_id = Column(Integer)
