9e3385dd111ab2908b912de6997556cf4b7e60fb
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 15:04:25 +0700

    Update models.py

--- app/core/order/models.py
+++ app/core/order/models.py
@@ -6,31 +6,25 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import BIGINT, INTEGER, ENUM, TEXT, FLOAT
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-
 class Product(Base):
     __tablename__ = "products"
 
-    id: Mapped[int] = mapped_column(INTEGER, primary_key=True)
-    name: Mapped[str] = mapped_column(TEXT, unique=True)
+    id: Mapped[int] = mapped_column(BIGINT, primary_key=True)
+    name: Mapped[str] = mapped_column(TEXT, nullable=False)
     price: Mapped[Decimal] = mapped_column(FLOAT, nullable=False)
-
 
 class Order(Base):
     __tablename__ = "orders"
 
-    id: Mapped[int] = mapped_column(INTEGER, primary_key=True)
+    id: Mapped[int] = mapped_column(BIGINT, primary_key=True)
     user_id: Mapped[int] = mapped_column(BIGINT, nullable=False)
-    status: Mapped[OrderStatusEnum] = mapped_column(
-        ENUM(*[str(member) for member in OrderStatusEnum], name="order_status"))
-    products: Mapped[list["OrderedProduct"]] = relationship()
-
+    status: Mapped[str] = mapped_column(ENUM(OrderStatusEnum), nullable=False)
+    products: Mapped[list["OrderedProduct"]] = relationship("OrderedProduct")
 
 class OrderedProduct(Base):
     __tablename__ = "orders_products"
 
-    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"), primary_key=True)
-    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), primary_key=True)
-    amount: Mapped[int] = mapped_column(INTEGER, nullable=False)
-
-    product: Mapped[Product] = relationship()
-
+    order_id: Mapped[int] = mapped_column(BIGINT, ForeignKey("orders.id"), primary_key=True)
+    product_id: Mapped[int] = mapped_column(BIGINT, ForeignKey("products.id"), primary_key=True)
+    quantity: Mapped[int] = mapped_column(INTEGER, nullable=False)
+    product: Mapped["Product"]] = relationship("Product")
