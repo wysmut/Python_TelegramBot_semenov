4e9721129ce8274f47aed2dcdc230c8eb840e9d6
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 18:06:14 +0700

    Update tests.py

--- orders/tests.py
+++ orders/tests.py
@@ -1,1 +1,8 @@
+from django.test import TestCase
+from .models import Order
 
+class OrderModelTest(TestCase):
+    def test_create_order(self):
+        order = Order.objects.create(description="Test Order", amount=100.00)
+        self.assertEqual(order.description, "Test Order")
+        self.assertEqual(order.amount, 100.00)
