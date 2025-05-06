3fc0a4607fae37d8bee2ee2666a0b04a098f0d46
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:10:50 +0700

    Update database.py

--- bot/utils/database.py
+++ bot/utils/database.py
@@ -1,18 +1,8 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from bot.secrets import DATABASE_URL
-from bot.models.models import Base
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
+Base = declarative_base()
