10046f2a2459f84900faec81771865b635cfbe17
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:20:52 +0700

    Update urls.py

--- django_app/app/urls.py
+++ django_app/app/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import EventViewSet
+
+router = DefaultRouter()
+router.register(r'events', EventViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
