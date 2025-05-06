f36086b2cdfba1be9cf37a832cda5c63354878e1
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 18:06:03 +0700

    Update serializers.py

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,1 +1,7 @@
+from rest_framework import serializers
+from .models import Order
 
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'description', 'amount', 'created_at']
