e97c155d52cccb5d38d576b11e8e7c3c095b21fc
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:10:03 +0700

    Update admin.py

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -1,1 +1,8 @@
+from aiogram import types, Dispatcher
+from aiogram.dispatcher.filters import Command
 
+async def admin_panel(message: types.Message):
+    await message.answer("Админ панель")
+
+def register_handlers_admin(dp: Dispatcher):
+    dp.register_message_handler(admin_panel, Command("admin"), is_admin=True)
