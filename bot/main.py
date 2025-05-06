1159784a5abb49d6b732638ed4e8ef8f7127593f
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:23:06 +0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,11 +3,14 @@ from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from bot.handlers import common, calendar, admin
 from bot.secrets import TELEGRAM_TOKEN
+from bot.utils.database import engine, Base
 
 logging.basicConfig(level=logging.INFO)
 storage = MemoryStorage()
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot, storage=storage)
+
+Base.metadata.create_all(bind=engine)
 
 common.register_handlers_common(dp)
 calendar.register_handlers_calendar(dp)
