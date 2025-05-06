1f3c84ba248a6a767f6d5a2fd4e048a9b13271a4
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:22:28 +0700

    Update database.py

--- bot/utils/database.py
+++ bot/utils/database.py
@@ -6,3 +6,10 @@ from bot.secrets import DATABASE_URL
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
