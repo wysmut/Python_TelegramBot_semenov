4236fd2d253bccf65496f9c24529b65bbcbf962b
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:16:23 +0700

    Update test_views.py

--- django_app/app/tests/test_views.py
+++ django_app/app/tests/test_views.py
@@ -1,1 +1,8 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import Event
 
+class EventViewTest(TestCase):
+    def test_event_list(self):
+        response = self.client.get(reverse('event-list'))
+        self.assertEqual(response.status_code, 200)
