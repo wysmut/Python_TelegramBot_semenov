ba1f000b43521dbed280279ed49dac30e41a6715
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 16:36:46 +0700

    Create settings.py

---
+++ django_app/app/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'calendar_bot',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
