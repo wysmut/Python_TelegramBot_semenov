9936fa3ff17a0eebf373a30ac893d6c60cc8fea2
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:16:13 +0700

    Update test_models.py

--- django_app/app/tests/test_models.py
+++ django_app/app/tests/test_models.py
@@ -1,1 +1,7 @@
+from django.test import TestCase
+from .models import Event
 
+class EventModelTest(TestCase):
+    def test_event_creation(self):
+        event = Event.objects.create(title="Test", date="2023-01-01", user_id=1)
+        self.assertEqual(event.title, "Test")
