b0968a648db51a904b9aee84f593c0f1afe4a839
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 15:03:10 +0700

    Update services.py

--- app/core/order/services.py
+++ app/core/order/services.py
@@ -5,7 +5,6 @@ from app.core.orders.exceptions import ActiveOrderExists
 from app.core.orders.models import Order, Product
 from app.core.orders.repositories import OrderRepository, ProductRepository
 
-
 @dataclass
 class ProductService:
     repository: ProductRepository
@@ -13,27 +12,24 @@ class ProductService:
     async def list_products(self) -> list[Product]:
         return await self.repository.list_products()
 
-
 @dataclass
 class OrderService:
-    repository: OrderRepository
+    order_repository: OrderRepository
+    product_repository: ProductRepository
 
-    async def create_order(self, user_id: int) -> int:
-        if await self.get_active_order_for_user(user_id):
+    async def create_order(self, user_id: int) -> Order:
+        if await self.order_repository.get_active_order(user_id):
             raise ActiveOrderExists()
-        return await self.repository.create_order(user_id)
+        return await self.order_repository.create_order(user_id)
 
-    async def get_order_by_id(self, order_id: int) -> Order | None:
-        return await self.repository.get_order_by_id(order_id)
-
-    async def get_active_order_for_user(self, user_id: int) -> Order | None:
-        return await self.repository.get_active_order_for_user(user_id)
+    async def get_active_order(self, user_id: int) -> Order | None:
+        return await self.order_repository.get_active_order(user_id)
 
     async def add_product_to_order(self, order_id: int, product_id: int) -> None:
-        return await self.repository.add_product_to_order(order_id, product_id)
+        await self.order_repository.add_product_to_order(order_id, product_id)
 
-    async def send_order_to_waiters(self, order_id: int) -> None:
-        return await self.repository.set_order_status(order_id, OrderStatusEnum.ordered)
+    async def finish_order(self, order_id: int) -> None:
+        await self.order_repository.finish_order(order_id)
 
-    async def mark_order_done(self, order_id: int) -> None:
-        return await self.repository.set_order_status(order_id, OrderStatusEnum.done)
+    async def complete_order(self, order_id: int) -> None:
+        await self.order_repository.complete_order(order_id)
