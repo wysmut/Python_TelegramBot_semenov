84576151e1adaeeec10ae9a54f3db7bab6e8fc04
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:15:43 +0700

    Update views.py

--- django_app/app/views.py
+++ django_app/app/views.py
@@ -1,29 +1,8 @@
-from rest_framework import viewsets, permissions
-from rest_framework.response import Response
-from .models import CalendarEvent, EventParticipation, UserStats
-from .serializers import CalendarEventSerializer, EventParticipationSerializer, UserStatsSerializer
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Event
+from .serializers import EventSerializer
 
-class CalendarEventViewSet(viewsets.ModelViewSet):
-    queryset = CalendarEvent.objects.all()
-    serializer_class = CalendarEventSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    
-    def get_queryset(self):
-        user = self.request.user
-        return CalendarEvent.objects.filter(owner=user) | CalendarEvent.objects.filter(participants=user)
-
-class EventParticipationViewSet(viewsets.ModelViewSet):
-    queryset = EventParticipation.objects.all()
-    serializer_class = EventParticipationSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    
-    def get_queryset(self):
-        return self.queryset.filter(user=self.request.user)
-
-class UserStatsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = UserStats.objects.all()
-    serializer_class = UserStatsSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    
-    def get_queryset(self):
-        return self.queryset.filter(user=self.request.user)
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
