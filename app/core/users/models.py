9815ae7000ef2f3e75065b277a74bfb26795a580
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 15:05:46 +0700

    Update models.py

--- app/core/users/models.py
+++ app/core/users/models.py
@@ -3,11 +3,8 @@ from app.infra.postgres.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import BIGINT, BOOLEAN
 
-
 class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(BIGINT, primary_key=True)
-    is_waiter: Mapped[bool] = mapped_column(BOOLEAN,
-                                            nullable=False,
-                                            default=False)
+    is_waiter: Mapped[bool] = mapped_column(BOOLEAN, nullable=False, default=False)
