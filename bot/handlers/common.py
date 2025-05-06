0b33b9c67b957c6065e9bb2808ce3d90622b9306
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 17:09:24 +0700

    Update common.py

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -1,1 +1,15 @@
+from aiogram import types, Dispatcher
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Command
 
+async def cmd_start(message: types.Message, state: FSMContext):
+    await state.finish()
+    await message.answer("Добро пожаловать!")
+
+async def cmd_cancel(message: types.Message, state: FSMContext):
+    await state.finish()
+    await message.answer("Действие отменено")
+
+def register_handlers_common(dp: Dispatcher):
+    dp.register_message_handler(cmd_start, commands="start", state="*")
+    dp.register_message_handler(cmd_cancel, commands="cancel", state="*")
