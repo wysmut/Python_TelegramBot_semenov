614601b4fbf26d8189df6b5ace470af5901678e8
Dmitrii Semenov <info@dxyzsem.ru>
2025-05-06 14:33:44 +0700

    Update commands.py

--- app/handlers/commands.py
+++ app/handlers/commands.py
@@ -10,7 +10,7 @@ from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.effective_chat and update.effective_user:
-        await context.application.user_service.register_visitor(update.effective_user.id)  # type: ignore[attr-defined]
+        await context.application.user_service.register_visitor(update.effective_user.id)
         keyboard = [
             [InlineKeyboardButton("Сделать заказ", callback_data=("order_create",))],
         ]
